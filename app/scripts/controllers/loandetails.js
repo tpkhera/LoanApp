5c64459285bbea244d0cfabd163b970ec70995e5
Tarun Khera <tpkhera@gmail.com>
2014-12-17 02:38:31 +0530

    meh

--- app/scripts/controllers/loandetails.js
+++ app/scripts/controllers/loandetails.js
@@ -9,16 +9,30 @@
  */
 angular.module('loanAdvisorApp')
   .controller('LoandetailsCtrl', function ($scope, $rootScope, $routeParams, sharedProps) {
-//    var loanId = $routeParams.loanID;
+    //    var loanId = $routeParams.loanID;
 
     $scope.loan = $rootScope.loan;
 
-//    sharedProps.getLoans()
-//    .then(function (response) {
-//      $scope.loan = sharedProps.getLoanId(response.data, loanId);
-//    });
+    //    sharedProps.getLoans()
+    //    .then(function (response) {
+    //      $scope.loan = sharedProps.getLoanId(response.data, loanId);
+    //    });
 
     $scope.principal = $rootScope.principal;
     $scope.time = $rootScope.time;
 
+    $scope.initial = function (bankName) {
+      return bankName.charAt(0);
+    };
+
+    $scope.EMI = function() {
+      var p = $scope.principal, r = $scope.loan.rate/1200, t = $scope.time*12;
+      return sharedProps.calculateEMI(p, r, t);
+    };
+
+
+
+    $scope.netInterest = function() {
+      return $scope.EMI() * $scope.time * 12 - $scope.principal;
+    };
   });
