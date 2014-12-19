dd18f1b76552b5b75819a42fe0fbda77d0e11b00
Tarun Khera <tpkhera@gmail.com>
2014-12-19 16:29:40 +0530

    login functionality!!

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,23 +8,73 @@
  * Controller of the loanAdvisorApp
  */
 angular.module('loanAdvisorApp')
-  .controller('LoginCtrl', function ($scope, $location) {
+  .controller('LoginCtrl', function ($scope, $location, $q, $mdToast, Auth) {
     $scope.user = {
       email: '',
       password: ''
     };
 
-    $scope.SignedUp = false;
-    //
-    //    $scope.changeForm = function() {
-    //      $scope.SignedUp = $scope.SignedUp === 0 ? 1 : 0;
-    //      console.log($scope.SignedUp);
-    //      $location.path('/');
-    //    };
+    $scope.authObj = Auth;
 
-    $scope.signIn = function (user) {
+    $scope.signIn = function () {
       if ($scope.loginForm.$valid) {
-        $location.path('/home');
+
+        $scope.tryLogIn().then(function (response) {
+          $scope.showSimpleToast('Signed in as ' + response.password.email);
+          $scope.goHome();
+        }, function (error) {
+          if (error.code === 'INVALID_USER') {
+            $scope.createUser().then(function () {
+              $scope.signIn();
+            });
+          } else if (error.code === 'INVALID_PASSWORD') {
+            $scope.showSimpleToast('Incorrect Password!');
+            $scope.user.password = '';
+          } else {
+            $scope.showSimpleToast('Invalid Email ID!');
+            console.error(error);
+            $scope.user.email = '';
+            $scope.user.password = '';
+          }
+        });
+
       }
     };
-  });
+
+  $scope.goHome = function() {
+    $location.path('/home');
+  };
+
+    $scope.tryLogIn = function () {
+      var deferred = $q.defer(),
+        loginPromise = $scope.authObj.$authWithPassword({
+          email: $scope.user.email,
+          password: $scope.user.password
+        });
+
+      loginPromise.then(function (authData) {
+        deferred.resolve(authData);
+      }).catch(function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+
+    $scope.createUser = function () {
+      return $scope.authObj.$createUser($scope.user.email, $scope.user.password);
+    };
+
+    $scope.showSimpleToast = function (message) {
+      $mdToast.show(
+        $mdToast.simple()
+        .content(message)
+        .position('bottom fit')
+        .hideDelay(5000)
+      );
+    };
+  })
+  .factory('Auth', ['$firebaseAuth',
+    function ($firebaseAuth) {
+      var ref = new Firebase('https://amber-torch-5533.firebaseio.com/');
+      return $firebaseAuth(ref);
+  }]);
